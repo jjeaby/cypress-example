5abc21c4d96295f12a57cab96bc18f587b78e06b
jin <jjeaby@gmail.com>
2021-06-04 01:18:10 +0900

    google 검색 cypress 로

--- cypress/integration/google-search.spec.js
+++ cypress/integration/google-search.spec.js
@@ -2,19 +2,20 @@ require('cypress-xpath')
 
 const CYPRESS_ENV = Cypress.env
 
-describe('구글 검색', () => {
-    before("Marimba Page Connect", () => {
+describe('구글에서 marimba.team 검색', () => {
+    before("구글 검색 페이지 접", () => {
         cy.intercept('/*').as('googleSearch')
         cy.visit(CYPRESS_ENV('BASE_URL'));
         cy.wait('@googleSearch').then((interception) => {
-            cy.xpath('//div/div/input').type('marimba.team')
-            cy.xpath('//center/input[]').click()
+            assert.isNotNull(interception.response.body, 'Google Search Page Loading')
         })
      });
 
 
-    it('Marimba Login', () => {
-         // cy.contains("")
+    it('marimba.team 검색', () => {
+        cy.xpath('//input[@name="q"]').type('marimba.team')
+        cy.contains('Google 검색').click({force: true});
+        cy.contains('Now use emojis to show your support to your team. Never get lost with. Real-time Cursor and ... Teaming. Productivity. Never miss a beat. Try Marimba for FREE.').should('exist');
     });
 
 });
